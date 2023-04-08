d3805f23c0ecd69d2945931e791f75992cd5a2f8
jesinthan0703 <jesinthan.23cs@licet.ac.in>
2023-04-08 21:04:35 +0530

    processing time calculation

--- routes/detector.py
+++ routes/detector.py
@@ -1,16 +1,19 @@
 from app import app
 from error_handles import forbidden
 from flask import Flask, jsonify, request, render_template
+import time
 
 
 @app.route('/vulnerability-check', methods=['POST', 'GET'])
 def getCode():
     # Sample route with a method handlers and a view function
+    start_time = time.time()
     try:
+        
         text = request.form['myText']
         processed_text = text.upper()
-        #print(processed_text)
-        return jsonify({'processed_text': processed_text})
+        end_time = time.time()
+        return jsonify({'processed_text': processed_text, 'result': f"Changes : Lower to upper case \nTime Taken : {end_time-start_time}"})
 
     except Exception as e:
         print(e)
